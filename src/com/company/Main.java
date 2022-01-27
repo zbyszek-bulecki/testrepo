508387fcec46ab4962029ab06368d0348eec98ad
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2022-01-27 20:48:24 +0100

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write a sentence:");
+        String sentence = scanner.nextLine();
+
+        Main spinWords = new Main();
+
+        System.out.printf(spinWords.spinWords(sentence));
+
+    }
+
+    public String spinWords(String sentence){
+
+       String[] tempArray = sentence.split(" ");
+       String result = "";
+
+       for (int i = 0 ; i < tempArray.length; i++ ) {
+           String tmpString = tempArray[i];
+           String potentiallyReversedTmpString = reverseIfNeeded(tmpString);
+      // for (String tmpString: tempArray) { //  for each loop
+           result += potentiallyReversedTmpString;
+           if (i < tempArray.length - 1) {
+               result += " ";
+           }
+       }
+
+        return result;
+    }
+
+    public String reverseIfNeeded(String word) {
+
+        if (word.length() < 5) {
+            return word;
+        }
+
+        StringBuilder maciek = new StringBuilder(word); /// TODO: 26/01/2022 get rid of StringBuilder and use a loop 
+        return maciek.reverse().toString();
+    }
+
+}
